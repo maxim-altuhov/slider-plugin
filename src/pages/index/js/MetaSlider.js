2bcb1bc85ca1ab2c1ae323811a61cc5f2d248b66
Maxim A <m_altuhov@mail.ru>
2021-08-26 19:10:11 +0300

    Добавил возможность настройки цветов слайдера и частично настроил поведение и проверку вход. значений слайдера при вкл. диапазоне значений

--- src/pages/index/js/MetaSlider.js
+++ src/pages/index/js/MetaSlider.js
@@ -1,20 +1,29 @@
+/* eslint-disable max-len */
 /* eslint-disable fsd/no-function-declaration-in-event-listener */
 class MetaSlider {
   selector = document.querySelector('#slider');
 
-  initValueLeft = 0;
-  initValueRight = 0;
-
-  minValue = -100;
-  maxValue = 100;
-  numberOfDivisions = 5;
+  mainColor = '#6d6dff';
+  secondColor = '#e4e4e4';
+  colorMarker = this.mainColor;
+  colorTextForMarker = '#ffffff'
+  colorForScale = '#000000'
 
   showError = true;
   showMinAndMaxValue = false;
   showTheScale = true;
   showMarkers = true;
   showBackgroundForRange = true;
-  isRange = false;
+  isRange = true;
+
+  minValue = 0;
+  maxValue = 100;
+  numberOfDivisions = 10;
+
+  initValueLeft = 50;
+  initValueRight = 50;
+  checkedInitValueLeft = this.isRange ? this.initValueLeft : this.minValue;
+  initValuesArray;
 
   constructor() {
     this.renderSlider();
@@ -24,7 +33,7 @@ class MetaSlider {
   }
 
   init() {
-    this.setValueForThumbs(this.initValueRight);
+    this.setValueForThumbs(this.initValuesArray);
     this.indicateMinAndMaxValue();
     this.createScaleOfValues();
     this.setEventsSlider();
@@ -36,75 +45,75 @@ class MetaSlider {
   }
 
   renderErrorMessage(message) {
-    const elemErrorInfo = document.createElement('p');
-    elemErrorInfo.classList.add('error-info');
-    this.selector.append(elemErrorInfo);
-    elemErrorInfo.textContent = message;
+    if (this.showError) {
+      const elemErrorInfo = document.createElement('p');
+      elemErrorInfo.classList.add('error-info');
+      this.selector.append(elemErrorInfo);
+      elemErrorInfo.textContent = message;
+    }
+  }
+
+  resetInitValue() {
+    this.initValueRight = this.maxValue;
+    this.checkedInitValueLeft = this.minValue;
   }
 
   initValueCheck() {
-    if (this.showError) {
-      const errorMessage = {
-        initValueLeft: 'Ошибка в данных для левого ползунка слайдера. Значение бегунка установлено в значение по-умолчанию',
-        initValueRight: 'Ошибка в данных для правого ползунка слайдера. Значение бегунка установлено в значение по-умолчанию',
-        minAndMaxValue: 'Ошибка во входящих данных: Max значение установленное для слайдера меньше его Min значения!',
-        maxValue: 'Ошибка в данных для ползунка слайдера. Установленно Max возможное значение для слайдера.',
-        minValue: 'Ошибка в данных для ползунка слайдера. Установленно Min возможное значение для слайдера.',
-        numberOfDivisions: `Установите корректное значение кол-ва интервалов для отображения шкалы с делениями. Установлено ближайщее оптимальное значение равное ${this.numberOfDivisions}.`,
-      };
+    const errorMessage = {
+      initValue: 'Ошибка во входящих данных для бегунков слайдеров. Установлены значения по-умолчанию',
+      minAndMaxValue: 'Ошибка во входящих данных: Max значение установленное для слайдера меньше или равно его Min значению!',
+      numberOfDivisions: 'Установите корректное значение кол-ва интервалов для отображения шкалы с делениями. Установлено ближайщее оптимальное значение.',
+    };
+    let showError = true;
 
-      if (this.initValueLeft > this.initValueRight) {
-        this.initValueLeft = this.minValue;
-        this.renderErrorMessage(errorMessage.initValueLeft);
-      }
-
-      if (this.initValueRight < this.initValueLeft) {
-        this.initValueRight = this.maxValue;
-        this.renderErrorMessage(errorMessage.initValueRight);
-      }
-
-      if (this.minValue > this.maxValue) {
-        this.renderErrorMessage(errorMessage.minAndMaxValue);
-      }
-
-      if (this.initValueRight > this.maxValue) {
-        this.initValueRight = this.maxValue;
-        this.renderErrorMessage(errorMessage.maxValue);
-      } else if (this.initValueRight < this.minValue) {
-        this.initValueRight = this.minValue;
-        this.renderErrorMessage(errorMessage.minValue);
-      }
-
-      while (!this.checkCorrectNumberOfDivisions() || this.numberOfDivisions <= 0) {
-        this.numberOfDivisions += 1;
-        this.renderErrorMessage(errorMessage.numberOfDivisions);
-      }
+    if (this.checkedInitValueLeft > this.initValueRight) {
+      this.resetInitValue();
+      this.renderErrorMessage(errorMessage.initValue);
     }
+
+    if (this.minValue > this.maxValue || this.minValue === this.maxValue) {
+      this.renderErrorMessage(errorMessage.minAndMaxValue);
+    }
+
+    if (this.initValueRight > this.maxValue || this.checkedInitValueLeft > this.maxValue) {
+      this.resetInitValue();
+      this.renderErrorMessage(errorMessage.initValue);
+    } else if (this.initValueRight < this.minValue || this.checkedInitValueLeft < this.minValue) {
+      this.resetInitValue();
+      this.renderErrorMessage(errorMessage.initValue);
+    }
+
+    while (!this.checkCorrectNumberOfDivisions() || this.numberOfDivisions <= 0) {
+      this.numberOfDivisions += 1;
+      if (showError) this.renderErrorMessage(errorMessage.numberOfDivisions);
+      showError = false;
+    }
+
+    this.initValuesArray = [this.checkedInitValueLeft, this.initValueRight];
   }
 
   renderSlider() {
     const fragmentWithASlider = document.createDocumentFragment();
     const blockSlider = document.createElement('div');
     blockSlider.classList.add('meta-slider');
+    blockSlider.style.backgroundColor = this.secondColor;
+
     blockSlider.dataset.min = this.minValue;
     blockSlider.dataset.max = this.maxValue;
 
-    const HTMLBlockWithOneThumb = `<button class="meta-slider__thumb meta-slider__thumb_right" data-value=''>
-    <span class="meta-slider__marker meta-slider__marker_right" style="display:none"></span>
-    </button>`;
+    const propDisplay = this.isRange ? 'display:block' : 'display:none';
 
-    const HTMLBlockWithTwoThumb = `<button class="meta-slider__thumb meta-slider__thumb_left" data-value=''>
-    <span class="meta-slider__marker meta-slider__marker_left" style="display:none"></span>
+    const HTMLBlock = `<div class="meta-slider__progress"></div>
+    <span class="meta-slider__value meta-slider__value_min"></span>
+    <button class="meta-slider__thumb meta-slider__thumb_left" style="background-color:${this.mainColor}; ${propDisplay}" data-value="">
+      <span class="meta-slider__marker meta-slider__marker_left" style="background-color:${this.colorMarker}; color: ${this.colorTextForMarker}; display:none"></span>
     </button>
-    ${HTMLBlockWithOneThumb}`;
-
-    const checkIsRangeResult = this.isRange ? HTMLBlockWithTwoThumb : HTMLBlockWithOneThumb;
-
-    const HTMLBlockResult = `<span class="meta-slider__value meta-slider__value_min"></span>
-    ${checkIsRangeResult}
+    <button class="meta-slider__thumb meta-slider__thumb_right" style="background-color:${this.mainColor}" data-value="">
+      <span class="meta-slider__marker meta-slider__marker_right" style="background-color:${this.colorMarker}; color: ${this.colorTextForMarker}; display:none"></span>
+    </button>
     <span class="meta-slider__value meta-slider__value_max"></span>`;
 
-    blockSlider.innerHTML = HTMLBlockResult;
+    blockSlider.innerHTML = HTMLBlock;
 
     fragmentWithASlider.append(blockSlider);
     this.selector.append(fragmentWithASlider);
@@ -113,12 +122,10 @@ class MetaSlider {
   getInfoAboutSlider() {
     this.elemSlider = this.selector.querySelector('.meta-slider');
 
-    this.elemThumbs = this.elemSlider.querySelectorAll('.meta-slider__thumb');
-    this.elemThumbLeft = this.elemSlider.querySelector('.meta-slider__thumb_left');
-    this.elemThumbRight = this.elemSlider.querySelector('.meta-slider__thumb_right');
+    this.sliderProgress = this.selector.querySelector('.meta-slider__progress');
 
-    this.elemMarkerLeft = this.elemSlider.querySelector('.meta-slider__marker_left');
-    this.elemMarkerRight = this.elemSlider.querySelector('.meta-slider__marker_right');
+    this.elemThumbs = this.elemSlider.querySelectorAll('.meta-slider__thumb');
+    this.elemMarkers = this.elemSlider.querySelectorAll('.meta-slider__marker');
 
     this.elemWithMinValue = this.elemSlider.querySelector('.meta-slider__value_min');
     this.elemWithMaxValue = this.elemSlider.querySelector('.meta-slider__value_max');
@@ -127,7 +134,7 @@ class MetaSlider {
     this.linkInitMouseUp = this.handleInitMouseUp.bind(this);
 
     this.widthSlider = this.elemSlider.offsetWidth;
-    this.widthThumb = this.elemThumbRight.offsetWidth;
+    this.widthThumb = this.elemThumbs[1].offsetWidth;
     this.thumbWidthAsPercentage = ((this.widthThumb / 2) / this.widthSlider) * 100;
   }
 
@@ -165,6 +172,7 @@ class MetaSlider {
 
         const elemScalePoint = document.createElement('button');
         elemScalePoint.classList.add('meta-slider__scale-point');
+        elemScalePoint.style.color = this.colorForScale;
         elemScalePoint.textContent = currentValue;
 
         blockScale.append(elemScalePoint);
@@ -175,7 +183,6 @@ class MetaSlider {
       const resultArrayScalePoint = document.querySelectorAll('.meta-slider__scale-point');
 
       resultArrayScalePoint.forEach((scalePoint) => {
-        // eslint-disable-next-line max-len
         const currentValueAsPercentage = ((scalePoint.textContent - this.minValue) / (this.maxValue - this.minValue)) * 100;
         const widthScalePointAsPercentage = ((scalePoint.offsetWidth / 2) / this.widthSlider) * 100;
 
@@ -186,39 +193,69 @@ class MetaSlider {
     }
   }
 
-  setValueInMarker(value) {
-    this.elemMarkerRight.style.display = 'none';
+  setValueInMarker(values) {
+    this.elemSlider.style.marginTop = '';
 
     if (this.showMarkers) {
       this.elemSlider.style.marginTop = '45px';
-      this.elemMarkerRight.textContent = Math.round(value);
-      this.elemMarkerRight.style.display = 'block';
+
+      values.forEach((currentValue, index) => {
+        this.elemMarkers[index].textContent = Math.round(currentValue);
+        this.elemMarkers[index].style.display = 'block';
+      });
     }
   }
 
-  setBackgroundTheRange(value) {
+  setBackgroundTheRange(valuesAsPercentage) {
     if (this.showBackgroundForRange) {
-      const color = `linear-gradient(90deg, #6d6dff ${value}%, #e4e4e4 ${value}%)`;
-      this.elemSlider.style.background = color;
+      const settingForRange = `left: ${valuesAsPercentage[0]}%; right: ${100 - valuesAsPercentage[1]}%; background: ${this.mainColor};`;
+      this.sliderProgress.style = settingForRange;
     }
   }
 
-  setValueForSlider(value, valueAsPercentage) {
-    this.elemThumbs[0].dataset.value = Math.round(value);
-    this.elemThumbs[0].style.left = (valueAsPercentage - this.thumbWidthAsPercentage) + '%';
-    this.setBackgroundTheRange(valueAsPercentage);
-    this.setValueInMarker(value);
+  setValueForSlider(values, valuesAsPercentage) {
+    values.forEach((currentValue, index) => {
+      this.elemThumbs[index].dataset.value = Math.round(currentValue);
+      this.elemThumbs[index].style.left = (valuesAsPercentage[index] - this.thumbWidthAsPercentage) + '%';
+    });
+    this.setBackgroundTheRange(valuesAsPercentage);
+    this.setValueInMarker(values);
   }
 
-  setValueForThumbs(value) {
-    const valueAsPercentage = ((value - this.minValue) / (this.maxValue - this.minValue)) * 100;
+  setValueForThumbs(values) {
+    let valuesAsPercentage = [];
 
-    this.setValueForSlider(value, valueAsPercentage);
+    values.forEach(currentValue => {
+      const currentValueAsPersentage = ((currentValue - this.minValue) / (this.maxValue - this.minValue)) * 100;
+      valuesAsPercentage.push(currentValueAsPersentage);
+    });
+
+    this.setValueForSlider(values, valuesAsPercentage);
   }
 
   setValueFromScale(event) {
-    const value = event.target.textContent;
-    this.setValueForThumbs(value);
+    const clickValue = Number(event.target.textContent);
+    const [leftValue, rightValue] = this.elemThumbs;
+    const currentLeftValue = leftValue.dataset.value;
+    const currentRightValue = rightValue.dataset.value;
+
+    const leftThumbDiff = Math.abs(clickValue - currentLeftValue);
+    const rightThumbDiff = Math.abs(clickValue - currentRightValue);
+    const inRange = clickValue > currentLeftValue && clickValue < currentRightValue;
+
+    if (clickValue <= currentLeftValue) this.initValuesArray[0] = clickValue;
+
+    if (inRange && leftThumbDiff < rightThumbDiff) {
+      this.initValuesArray[0] = clickValue;
+    } else if (inRange && leftThumbDiff > rightThumbDiff) {
+      this.initValuesArray[1] = clickValue;
+    } else if (inRange && leftThumbDiff === rightThumbDiff) {
+      this.initValuesArray[1] = clickValue;
+    }
+
+    if (clickValue >= currentRightValue || !this.isRange) this.initValuesArray[1] = clickValue;
+
+    this.setValueForThumbs(this.initValuesArray);
   }
 
   setPositionForThumbs(event) {
