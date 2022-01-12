f4069dc96b31f94b0565325a8ccf04092aa25d50
Maxim A <m_altuhov@mail.ru>
2022-01-12 20:23:00 +0300

    Написал тесты для Model на 50%

--- src/pages/metaSlider/__tests__/layers/Model.test.ts
+++ src/pages/metaSlider/__tests__/layers/Model.test.ts
@@ -22,6 +22,7 @@ describe('Checking the "Model" layer', () => {
   });
 
   test('State before first initialization', () => {
+    expect($selector.html()).toMatchSnapshot();
     expect(classModel.opt).toBe(initSettings);
     expect(classModel.opt.$selector).toBe($selector);
     expect(classModel.observerList).toHaveLength(0);
@@ -130,7 +131,6 @@ describe('Checking the "Model" layer', () => {
       classModel.opt.numberOfDecimalPlaces = 0;
 
       classModel.calcTargetValue(null, checkingValue, true);
-
       const { minValue, maxValue } = classModel.opt;
 
       if (checkingValue < minValue) {
@@ -185,7 +185,14 @@ describe('Checking the "Model" layer', () => {
     classModel.opt.maxValue = 100;
 
     classModel['_checkingTargetValue'](TEST_VALUE);
-    const { initValueFirst, initValueSecond, minValue, maxValue } = classModel.opt;
+
+    // prettier-ignore
+    const { 
+      initValueFirst,
+      initValueSecond,
+      minValue,
+      maxValue,
+    } = classModel.opt;
 
     expect(initValueFirst).toBe(minValue);
     expect(initValueSecond).toBe(maxValue);
@@ -225,4 +232,121 @@ describe('Checking the "Model" layer', () => {
 
     expect(initValuesArray).toEqual([TEST_VALUE, initValueSecond]);
   });
+
+  test('Checking the "_calcValuesInPercentage" method', () => {
+    classModel.opt.minValue = 0;
+    classModel.opt.maxValue = 200;
+    classModel.opt.initValuesArray = [0, 100];
+
+    classModel['_calcValuesInPercentage']();
+    const { valuesAsPercentageArray } = classModel.opt;
+
+    expect(valuesAsPercentageArray).toEqual([0, 50]);
+  });
+
+  test('Checking the "_setCurrentValues" method', () => {
+    jest.spyOn(classModel, 'notify').mockImplementation();
+    classModel.opt.initValuesArray = [1, 2];
+    classModel.opt.customValues = [0, 100, 200];
+
+    classModel['_setCurrentValues']();
+
+    // prettier-ignore
+    const { 
+      key, 
+      initValuesArray,
+      initValueFirst,
+      initValueSecond,
+      textValueFirst,
+      textValueSecond,
+      textValuesArray,
+      customValues,
+    } = classModel.opt;
+
+    expect(key).toBe('changedValue');
+    expect(initValueFirst).toBe(initValuesArray[0]);
+    expect(initValueSecond).toBe(initValuesArray[1]);
+    expect(textValueFirst).toBe(String(customValues[initValuesArray[0]]));
+    expect(textValueSecond).toBe(String(customValues[initValuesArray[1]]));
+    expect(textValuesArray).toEqual([textValueFirst, textValueSecond]);
+    expect(classModel.notify).toHaveBeenCalledWith(classModel.opt);
+  });
+
+  test.each(['init', 'step', 'maxValue', 'minValue', 'customValues', 'numberOfDecimalPlaces'])(
+    'Checking the "_calcStepAsPercentage" method => key = %p',
+    (key) => {
+      classModel.opt.key = key;
+      classModel.opt.minValue = 0;
+      classModel.opt.maxValue = 100;
+      classModel.opt.step = 5;
+
+      classModel['_calcStepAsPercentage']();
+      const { stepAsPercent } = classModel.opt;
+
+      expect(stepAsPercent).toBe(5);
+    },
+  );
+
+  test('Checking the "_checkingIncomingProp" method', () => {
+    jest.spyOn<Model, any>(classModel, '_checkingIsVerticalSlider').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_checkingNumberOfDecimalPlaces').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_checkingMinMaxValues').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_checkingStepSize').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_checkingCustomValues').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_calcStepAsPercentage').mockImplementation();
+    jest.spyOn<Model, any>(classModel, '_checkingInitValues').mockImplementation();
+
+    classModel['_checkingIncomingProp']();
+
+    expect(classModel['_checkingIsVerticalSlider']).toHaveBeenCalled();
+    expect(classModel['_checkingNumberOfDecimalPlaces']).toHaveBeenCalled();
+    expect(classModel['_checkingMinMaxValues']).toHaveBeenCalled();
+    expect(classModel['_checkingStepSize']).toHaveBeenCalled();
+    expect(classModel['_checkingCustomValues']).toHaveBeenCalled();
+    expect(classModel['_calcStepAsPercentage']).toHaveBeenCalled();
+    expect(classModel['_checkingInitValues']).toHaveBeenCalled();
+  });
+
+  test('Checking the "_getSliderSelectors" method', () => {
+    const mockFind = jest.spyOn($.fn, 'find');
+    classModel.opt.$elemSlider = $();
+    classModel.opt.$sliderProgress = $();
+    classModel.opt.$elemThumbs = $();
+    classModel.opt.$elemMarkers = $();
+    classModel.opt.$elemScale = $();
+
+    expect(classModel.opt.$elemSlider).toHaveLength(0);
+    expect(classModel.opt.$sliderProgress).toHaveLength(0);
+    expect(classModel.opt.$elemThumbs).toHaveLength(0);
+    expect(classModel.opt.$elemMarkers).toHaveLength(0);
+    expect(classModel.opt.$elemScale).toHaveLength(0);
+
+    classModel['_getSliderSelectors']();
+
+    expect(mockFind).toBeCalledTimes(5);
+    expect(classModel.opt.$elemSlider).toHaveLength(1);
+    expect(classModel.opt.$sliderProgress).toHaveLength(1);
+    expect(classModel.opt.$elemThumbs).toHaveLength(2);
+    expect(classModel.opt.$elemMarkers).toHaveLength(2);
+    expect(classModel.opt.$elemScale).toHaveLength(1);
+  });
+
+  test.each(['init', 'initAutoMargins', 'isVertical'])(
+    'Checking the "_checkingIsVerticalSlider" method => isVertical, initAutoMargins = true/false => key = %p',
+    (key) => {
+      classModel.opt.key = key;
+
+      [true, false].forEach((status) => {
+        classModel.opt.isVertical = !status;
+        classModel.opt.initAutoMargins = status;
+        classModel['_checkingIsVerticalSlider']();
+
+        const { initAutoMargins } = classModel.opt;
+        const statusAutoMargins = classModel['_listSavedStatus']['autoMargins'];
+
+        expect(classModel['_listSavedStatus']).toHaveProperty('autoMargins', initAutoMargins);
+        expect(initAutoMargins).toBe(statusAutoMargins);
+      });
+    },
+  );
 });
